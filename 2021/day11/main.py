ca1abd97adda22adf6dc7698139439fe9e657826
David Kucher <mrdkucher@gmail.com>
2022-12-01 10:23:15 -0800

    cleanup day11

--- 2021/day11/main.py
+++ 2021/day11/main.py
@@ -11,10 +11,8 @@ def create_matrix(text):
 
     arr = np.zeros((10, 10))
     for i, line in enumerate(text):
-        # print('> ', line.rstrip('\n'))
         for j, char in enumerate(line.rstrip('\n')):
             arr[i, j] = int(char)
-    # print(arr)
     return arr
 
 
@@ -22,36 +20,20 @@ def single_step(matrix):
     matrix += 1
 
     mask = matrix > 9
-    # print("INIT > 9")
-    # print(mask.astype(int))
 
     kernel = np.ones((3, 3))
     kernel[1, 1] = 0
 
-
-    # print("KERNEL\n", kernel)
-
     counted_mask = mask
     while np.sum(mask) > 0:
         output = convolve2d(mask, kernel, mode='same')
-        # print("FILTERED OUTPUT\n", output)
         matrix = matrix + output
         mask = (matrix > 9) ^ counted_mask
-        # print('new mask:\n', mask.astype(int))
         counted_mask = (matrix > 9)
-        # print('counted\n', counted_mask.astype(int))
-        # print(np.sum(mask))
 
     mask = matrix > 9
-    # print("NEW MATRIX:", matrix)
-
-    # print("NEW > 9\n", mask.astype(int))
-
     num_flashes = np.sum(mask)
-
     matrix *= (1 - mask)
-    # print("AFTER STEP\n", matrix)
-    # print("================================================================")
 
     return matrix, num_flashes
 
@@ -73,24 +55,15 @@ def run_steps(matrix, count=100):
 
 
 def test():
-    # input = read_input("test2.txt")
-    # start_matrix = create_matrix(input)
-
-    # total_flashes = run_steps(start_matrix, 3)
-
-    # assert total_flashes == 9
-
     input = read_input("test.txt")
     start_matrix = create_matrix(input)
     start_matrix2= np.copy(start_matrix)
 
     total_flashes = run_steps(start_matrix, 100)
-    # print("TOTAL FLASHES: ", total_flashes)
 
     assert total_flashes == 1656
 
     steps = run_steps(start_matrix2, -1)
-
     assert steps == 195
 
 
@@ -104,8 +77,3 @@ if __name__ == "__main__":
     print("TOTAL FLASHES: ", total_flashes)
     steps = run_steps(matrix2, -1)
     print('SYNC STEPS:', steps)
-
-
-
-# input = read_input()
-# start_matrix = create_matrix(input)
